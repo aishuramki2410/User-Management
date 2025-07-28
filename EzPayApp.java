bacd6c9399da590615d7e73f70de3f42552c3100
aishuramki2410 <142071935+aishuramki2410@users.noreply.github.com>
2025-07-28 15:37:16 +0530

    Add files via upload

---
+++ EzPayApp.java
@@ -0,0 +1,9 @@
+package com.nw.ezpay;
+
+import com.nw.ezpay.controller.MainController;
+
+public class EzPayApp {
+    public static void main(String[] args) {
+        MainController.run();
+    }
+}
